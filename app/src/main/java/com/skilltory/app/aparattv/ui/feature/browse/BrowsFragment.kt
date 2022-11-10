da32882b288d137da89e69ff4315cc098ee3c747
full name <mostafa.imani.official@gmail.com>
2022-11-10 14:03:28 +0330

    Add HomePresenter (RecyclerView)

--- app/src/main/java/com/skilltory/app/aparattv/ui/feature/browse/BrowsFragment.kt
+++ app/src/main/java/com/skilltory/app/aparattv/ui/feature/browse/BrowsFragment.kt
@@ -10,8 +10,10 @@ import androidx.leanback.app.BackgroundManager
 import androidx.leanback.app.BrowseSupportFragment
 import androidx.leanback.widget.*
 import androidx.lifecycle.asLiveData
+import androidx.navigation.fragment.findNavController
 import com.skilltory.app.aparattv.R
 import com.skilltory.app.aparattv.domain.model.Category
+import com.skilltory.app.aparattv.domain.model.Video
 import com.skilltory.app.aparattv.utils.Constants
 import com.skilltory.app.aparattv.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,20 +22,39 @@ import dagger.hilt.android.AndroidEntryPoint
 class BrowsFragment:BrowseSupportFragment() {
 
     private val viewModel: BrowsViewModel by viewModels()
+    private val backgroundManager by lazy {
+        BackgroundManager.getInstance(requireActivity()).apply {
+            attach(requireActivity().window)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        title = getString(R.string.app_name)
 
+//        title = getString(R.string.app_name)
 
+        if (savedInstanceState == null) {
+//            prepareEntranceTransition()
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         observeData()
 
     }
 
+
+
+
+    override fun getSelectedPosition(): Int {
+        Log.i("Selecting", "getSelectedPosition: ${super.getSelectedPosition()}")
+        return super.getSelectedPosition().also {
+            Log.i("Selecting", "getSelectedPosition: $it")
+
+        }
+    }
 
     private fun observeData() {
         viewModel.videoResponse.asLiveData().observe(viewLifecycleOwner) { resource ->
@@ -43,7 +64,6 @@ class BrowsFragment:BrowseSupportFragment() {
                 is Resource.Loading -> {
                 }
                 is Resource.Success -> {
-                    Log.i(Constants.TAG_HOME, "observeData: ${resource.data.categoryList}")
                     displayData(resource.data.categoryList)
 //                    startEntranceTransition()
                 }
@@ -55,10 +75,20 @@ class BrowsFragment:BrowseSupportFragment() {
     }
 
 
+
     private fun displayData(categories: List<Category>) {
+        val adapter = ArrayObjectAdapter(ListRowPresenter())
+        for (category in categories) {
+            val headerItem = HeaderItem(category.id, category.genre)
+            val rowAdapter = ArrayObjectAdapter(VideoCardPresenter())
+            for (movie in category.movies) {
+                rowAdapter.add(movie)
+            }
+            adapter.add(ListRow(headerItem, rowAdapter))
+        }
+
+
+        this.adapter = adapter
 
     }
-
-
-
 }
---
+++ app/src/main/java/com/skilltory/app/aparattv/ui/feature/browse/VideoCardPresenter.kt
@@ -0,0 +1,56 @@
+package com.skilltory.app.aparattv.ui.feature.browse
+
+import android.util.Log
+import android.view.ViewGroup
+import androidx.leanback.widget.BaseCardView
+import androidx.leanback.widget.ImageCardView
+import androidx.leanback.widget.Presenter
+import coil.load
+import coil.size.Scale
+import com.skilltory.app.aparattv.R
+import com.skilltory.app.aparattv.domain.model.Video
+import com.skilltory.app.aparattv.utils.Constants
+
+class VideoCardPresenter : Presenter() {
+    override fun onCreateViewHolder(parent: ViewGroup): ViewHolder {
+
+        val imageCardView = ImageCardView(parent.context).apply {
+            isFocusable = true
+            isFocusableInTouchMode = true
+            cardType = BaseCardView.CARD_TYPE_MAIN_ONLY
+            with(mainImageView) {
+                val posterWidth = 250.toInt()
+                val posterHeight = 200.toInt()
+                layoutParams = BaseCardView.LayoutParams(posterWidth, posterHeight)
+            }
+        }
+        return ViewHolder(imageCardView)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, item: Any) {
+        val video = item as Video
+        with(viewHolder.view as ImageCardView) {
+            val posterWidth = 250.toInt()
+            val posterHeight = 200.toInt()
+
+            mainImageView.load(
+                uri = video.imageUrl,
+                builder = {
+                    scale(Scale.FIT)
+                    size(posterWidth, posterHeight)
+                    allowHardware(false)
+                })
+            titleText = video.title
+        }
+        viewHolder.view.tag = item.title
+        Log.i(Constants.TAG_HOME_RECYCELER, "onBindViewHolder: ${item.title}  View:-${viewHolder.view.tag} ")
+    }
+
+    override fun onUnbindViewHolder(viewHolder: ViewHolder) {
+        Log.i("Recycler", "onUnbindViewHolder: View:-${viewHolder.view?.tag} ")
+        with(viewHolder.view as ImageCardView) {
+            mainImage = null
+        }
+
+    }
+}
